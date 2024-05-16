3c71d27bba07fbd0be425c4b13ebb2df29b29e5d
이승준 <klsj9810@gmail.com>
2024-05-17 02:18:50 +0900

    회원가입 UI 디자인

--- src/components/auth/sign-up-form.tsx
+++ src/components/auth/sign-up-form.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import RouterLink from 'next/link';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { MenuItem, Select, Stack } from '@mui/material';
+import { FormLabel, MenuItem, Radio, RadioGroup, Select, Stack } from '@mui/material';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
@@ -29,11 +29,26 @@ const schema = zod.object({
   password: zod.string().min(6, { message: 'Password should be at least 6 characters' }),
   role: zod.string().min(1, { message: 'Role is required' }),
   terms: zod.boolean().refine((value) => value, 'You must accept the terms and conditions'),
+  concurrentUserCount: zod.string().min(1, { message: 'input number!' }),
+  availableRAM: zod.string().min(1, { message: 'check' }),
+  cpuType: zod.string().min(1, { message: 'check' }),
+  cpuCount: zod.string().min(1, { message: 'check' }),
 });
 
 type Values = zod.infer<typeof schema>;
 
-const defaultValues = { firstName: '', lastName: '', email: '', password: '', role: '', terms: false } satisfies Values;
+const defaultValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  role: '',
+  terms: false,
+  concurrentUserCount: '',
+  availableRAM: '',
+  cpuType: '',
+  cpuCount: '',
+} satisfies Values;
 
 export function SignUpForm(): React.JSX.Element {
   const router = useRouter();
@@ -47,6 +62,7 @@ export function SignUpForm(): React.JSX.Element {
     handleSubmit,
     setError,
     formState: { errors },
+    watch,
   } = useForm<Values>({ defaultValues, resolver: zodResolver(schema) });
 
   const onSubmit = React.useCallback(
@@ -70,6 +86,8 @@ export function SignUpForm(): React.JSX.Element {
     },
     [checkSession, router, setError]
   );
+
+  const role = watch('role');
 
   return (
     <Stack spacing={3}>
@@ -141,6 +159,76 @@ export function SignUpForm(): React.JSX.Element {
               </FormControl>
             )}
           />
+          {role === 'enterprise' && (
+            <Stack spacing={2}>
+              <Typography color="text.secondary" variant="body2">
+                Design your service performance
+              </Typography>
+              <Controller
+                control={control}
+                name="concurrentUserCount"
+                render={({ field }) => (
+                  <FormControl>
+                    <InputLabel>동시 사용자 수</InputLabel>
+                    <OutlinedInput {...field} type="number" label="동시 사용자 수" />
+                  </FormControl>
+                )}
+              />
+              <Controller
+                control={control}
+                name="availableRAM"
+                render={({ field }) => (
+                  <FormControl>
+                    <InputLabel>메모리</InputLabel>
+                    <Select {...field} label="메모리">
+                      <MenuItem value="1GiB">1 GiB 메모리</MenuItem>
+                      <MenuItem value="2GiB">2 GiB 메모리</MenuItem>
+                      <MenuItem value="4GiB">4 GiB 메모리</MenuItem>
+                      <MenuItem value="8GiB">8 GiB 메모리</MenuItem>
+                      <MenuItem value="16GiB">16 GiB 메모리</MenuItem>
+                      <MenuItem value="32GiB">32 GiB 메모리</MenuItem>
+                    </Select>
+                  </FormControl>
+                )}
+              />
+              <Controller
+                control={control}
+                name="cpuType"
+                render={({ field }) => (
+                  <FormControl>
+                    <FormLabel id="demo-radio-buttons-group-label">CPU 아키텍처</FormLabel>
+                    <RadioGroup
+                      row
+                      {...field}
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      defaultValue="64비트(x86)"
+                      name="radio-buttons-group"
+                    >
+                      <FormControlLabel value="64비트(x86)" control={<Radio />} label="64비트(x86)" />
+                      <FormControlLabel value="64비트(Arm)" control={<Radio />} label="64비트(Arm)" />
+                    </RadioGroup>
+                  </FormControl>
+                )}
+              />
+              <Controller
+                control={control}
+                name="cpuCount"
+                render={({ field }) => (
+                  <FormControl>
+                    <InputLabel>가상 CPU 수</InputLabel>
+                    <Select {...field} label="가상 CPU 수">
+                      <MenuItem value="1vCPU">1 vCPU</MenuItem>
+                      <MenuItem value="2vCPU">2 vCPU</MenuItem>
+                      <MenuItem value="4vCPU">4 vCPU</MenuItem>
+                      <MenuItem value="8vCPU">8 vCPU</MenuItem>
+                      <MenuItem value="16vCPU">16 vCPU</MenuItem>
+                    </Select>
+                  </FormControl>
+                )}
+              />
+            </Stack>
+          )}
+
           <Controller
             control={control}
             name="terms"
